db021f5fc60f1a4a2ec67f395e2feb400e9e6c23
madcampos <madcampos@outlook.com>
2022-11-18 01:28:44 -0500

    feat: update select element

--- src/components/select/select.ts
+++ src/components/select/select.ts
@@ -1,88 +1,82 @@
+import selectTemplate from './select.html?raw';
+import selectStyle from './select.css?raw';
+
 import { AdamElement } from '../AdamElement/AdamElement';
 
+const watchedAttributes = ['label', 'required', 'disabled', 'multiple', 'size', 'value'];
+
+interface AdamSelect {
+	required: boolean,
+	disabled: boolean,
+	multiple: boolean,
+	size: number,
+	value: string
+}
+
 class AdamSelect extends AdamElement {
-	// eslint-disable-next-line @typescript-eslint/class-literal-property-style
-	static get formAssociated() { return true; }
-	static get observedAttributes() { return ['label', 'required', 'disabled', 'multiple', 'size', 'placeholder', 'value']; }
+	static observedAttributes = watchedAttributes;
 	#select: HTMLSelectElement;
 
 	constructor() {
 		super({
 			name: 'adam-select',
-			watchedProps: [
-				{ prop: 'required', type: 'boolean', defaultValue: false },
-				{ prop: 'disabled', type: 'boolean', defaultValue: false },
-				{ prop: 'multiple', type: 'boolean', defaultValue: false },
-				{ prop: 'size', type: 'number', defaultValue: 0 },
-				{ prop: 'value', type: 'string', defaultValue: '' }
+			props: [
+				{ name: 'required', value: false },
+				{ name: 'disabled', value: false },
+				{ name: 'multiple', value: false },
+				{ name: 'size', value: 0 },
+				{
+					name: 'value',
+					value: (value?: string) => {
+						this.#select.value = value ?? '';
+
+						if (!this.#select.checkValidity()) {
+							// @ts-expect-error
+							this.root.querySelector('#error-text')?.innerText = this.#select.validationMessage;
+						}
+
+						return this.#select.value;
+					}
+				}
 			],
-			style: import.meta.url,
-			template: `
-				<div>
-					<select id="{id}" name="{id}">
-						<option selected disabled hidden>{placeholder}</option>
-					</select>
-					<div id="outer-border"></div>
-					<div id="attention-marker"></div>
-					<label for="{id}">
-						{label}
-						<small id="required-text">(required)</small>
-					</label>
-					<div id="info-box">
-						<div id="info-text">
-							<slot name="helper-text"></slot>
-						</div>
-						<div id="error-text"></div>
-					</div>
-					<div hidden>
-						<slot></slot>
-					</div>
-				</div>
-			`
-		});
+			style: selectStyle,
+			template: selectTemplate,
+			watchedAttributes,
+			watchedSlots: {
+				'default': (evt) => {
+					const target = evt.target as HTMLSlotElement;
+					const optionList = [...target.assignedElements()].filter((option) => option instanceof HTMLOptGroupElement || option instanceof HTMLOptionElement);
 
-		this.root.addEventListener('slotchange', (evt) => {
-			const target = evt.target as HTMLSlotElement;
+					for (const option of optionList) {
+						this.#select.add(option as HTMLOptionElement | HTMLOptGroupElement);
+					}
+				}
+			},
+			handlers: {
+				change: (evt: Event) => {
+					const target = evt.target as HTMLSelectElement;
 
-			if (!target.name) {
-				const optionList = [...target.assignedElements()].filter((option) => option instanceof HTMLOptGroupElement || option instanceof HTMLOptionElement);
+					target.checkValidity();
 
-				for (const option of optionList) {
-					this.#select.add(option as HTMLOptionElement | HTMLOptGroupElement);
+					this.internals.setFormValue(target.value);
+					this.dispatchEvent(new CustomEvent('change', { bubbles: true, composed: true }));
+				},
+				invalid: (evt: Event) => {
+					evt.preventDefault();
+					evt.stopPropagation();
+
+					const errorText = this.root.querySelector('#error-text') as HTMLDivElement;
+					const target = evt.target as HTMLSelectElement;
+
+					errorText.innerText = target.validationMessage;
+
+					this.internals.setValidity(this.#select.validity);
+					this.dispatchEvent(new CustomEvent('invalid', { bubbles: true, composed: true }));
 				}
 			}
 		});
 
 		this.#select = this.root.querySelector('select') as HTMLSelectElement;
-	}
-
-	mounted() {
-		if (!this.#select.checkValidity()) {
-			// @ts-expect-error
-			this.root.querySelector('#error-text')?.innerText = this.#select.validationMessage;
-		}
-
-		this.#select.addEventListener('change', (evt) => {
-			const target = evt.target as HTMLSelectElement;
-
-			target.checkValidity();
-
-			this.internals.setFormValue(this.#select.value);
-			this.dispatchEvent(new CustomEvent('change', { bubbles: true, composed: true }));
-		});
-
-		this.#select.addEventListener('invalid', (evt) => {
-			evt.preventDefault();
-			evt.stopPropagation();
-
-			const errorText = this.root.querySelector('#error-text') as HTMLDivElement;
-			const target = evt.target as HTMLSelectElement;
-
-			errorText.innerText = target.validationMessage;
-
-			this.internals.setValidity(this.#select.validity);
-			this.dispatchEvent(new CustomEvent('invalid', { bubbles: true, composed: true }));
-		});
 	}
 }
 
